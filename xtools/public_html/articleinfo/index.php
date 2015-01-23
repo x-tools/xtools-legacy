4c16c3a4623b39e79896ef2cefe1310f85224f81
Maximilian Doerr <cybernet678@yahoo.com>
2015-01-23 14:59:28 -0500

    Fix redirect scripts
    Syntax error caused a blank page to load instead of a redirect.

--- xtools/public_html/articleinfo/index.php
+++ xtools/public_html/articleinfo/index.php
@@ -12,7 +12,7 @@ function delayer(){
 </head>
 <body onLoad="setTimeout('delayer()', 5000)">
 <h1>301 Moved Permanently</h1>
-<p>This tool has moved to a new location.  You will be redirected to tools.wmflabs.org/xtools-articleinfo/index.php?<?php echo $_SERVER['QUERY_STRING']; ? shortly.</p>
+<p>This tool has moved to a new location.  You will be redirected to tools.wmflabs.org/xtools-articleinfo/index.php?<?php echo $_SERVER['QUERY_STRING']; ?> shortly.</p>
 
 </body>
 </html>
